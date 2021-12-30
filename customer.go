c3c419eec96a76c9ca4aad11d688c93122bc56c2
Sacrif <sacrifw@gmail.com>
2021-12-30 15:36:09 +0200

    ok

--- customer.go
+++ customer.go
@@ -36,14 +36,32 @@ func (u UserModule) Create() *UserModule {
 }
 
 func (u *UserModule) GetBook(c *gin.Context) {
-	user := User{} // получать юзера
-	book := Book{}
-	if err := c.Bind(&book); err != nil {
+	inputData := struct {
+		UserId string `json:"userId"`
+		BookId string `json:"bookId"`
+	}{}
+
+	if err := c.Bind(&inputData); err != nil {
 		fmt.Println("customer.go -> GetBook -> Bind: err = ", err)
 		c.JSON(http.StatusInternalServerError, obj{"error": "wrong"})
 		return
 	}
-	// смотрим книгу в базе данных, проверяем на занятость
+	user := User{}
+	book := Book{}
+	err := storage.C("users").FindId(bson.ObjectId(inputData.UserId)).One(&user)
+	if err != nil {
+		fmt.Println("customer.go -> GetBook -> user not found, err:", err)
+		c.JSON(http.StatusNotFound, obj{"error": "user not found"})
+		return
+	}
+
+	err = storage.C("books").FindId(bson.ObjectId(inputData.BookId)).One(&book)
+	if err != nil {
+		fmt.Println("customer.go -> GetBook -> book not found, err:", err)
+		c.JSON(http.StatusNotFound, obj{"error": "book not found"})
+		return
+	}
+	// проверяем на занятость
 	if book.TakenBy != "" {
 		c.JSON(http.StatusBadRequest, obj{"error": "Book already taken"})
 		return
@@ -51,20 +69,40 @@ func (u *UserModule) GetBook(c *gin.Context) {
 	// добавляем в список
 	user.Books = append(user.Books, book)
 	//сохраняем в базу юзеров изменения.
+	storage.C("users").Update(user.Id, user)
 
 	book.TakenBy = user.TicketNumber
+	book.ReturnDate = time.Now().AddDate(0, 0, 30) // 30 дней с момента взятия книги
 	// сохраняем в базу книг
+	storage.C("books").Update(book.Id, book)
 
 	c.JSON(http.StatusOK, obj{"answer": "ok"})
 
 }
 
 func (u *UserModule) ReturnBook(c *gin.Context) {
-	user := User{} // получать юзера
-	book := Book{}
-	if err := c.Bind(&book); err != nil {
-		fmt.Println("customer.go -> ReturnBook -> Bind: err = ", err)
+	inputData := struct {
+		UserId string `json:"userId"`
+		BookId string `json:"bookId"`
+	}{}
+	if err := c.Bind(&inputData); err != nil {
+		fmt.Println("customer.go -> GetBook -> Bind: err = ", err)
 		c.JSON(http.StatusInternalServerError, obj{"error": "wrong"})
+		return
+	}
+
+	user := User{}
+	book := Book{}
+	err := storage.C("users").FindId(bson.ObjectId(inputData.UserId)).One(&user)
+	if err != nil {
+		fmt.Println("customer.go -> GetBook -> user not found, err:", err)
+		c.JSON(http.StatusNotFound, obj{"error": "user not found"})
+		return
+	}
+	err = storage.C("books").FindId(bson.ObjectId(inputData.BookId)).One(&book)
+	if err != nil {
+		fmt.Println("customer.go -> GetBook -> book not found, err:", err)
+		c.JSON(http.StatusNotFound, obj{"error": "book not found"})
 		return
 	}
 	userBooksNew := []Book{}
@@ -75,15 +113,39 @@ func (u *UserModule) ReturnBook(c *gin.Context) {
 	}
 	user.Books = userBooksNew
 	// сохраняем в базу юзеров
+	storage.C("users").Update(user.Id, user)
 
 	book.TakenBy = ""
 	//сохраняем в базу книг
+	storage.C("books").Update(book.Id, book)
 
 	c.JSON(http.StatusOK, obj{"answer": "ok"})
 }
 
-func (u *UserModule) GetAllTakenBooks() {
+// получить список книг у читателя
+func (u *UserModule) GetAllTakenBooks(c *gin.Context) {
+	inputData := struct {
+		UserId string `json:"userId"`
+	}{}
+	if err := c.Bind(&inputData); err != nil {
+		fmt.Println("customer.go -> GetAllTakenBooks -> Bind: err = ", err)
+		c.JSON(http.StatusInternalServerError, obj{"error": "wrong"})
+		return
+	}
+	user := User{}
+	err := storage.C("users").FindId(bson.ObjectId(inputData.UserId)).One(&user)
+	if err != nil {
+		fmt.Println("customer.go -> GetBook -> user not found, err:", err)
+		c.JSON(http.StatusNotFound, obj{"error": "user not found"})
+		return
+	}
+	booksList := []string{}
+	for _, book := range user.Books {
+		// записываем список книг с авторами, которые на руках у данного пользователя
+		booksList = append(booksList, book.Name+", "+book.Author)
+	}
 
+	c.JSON(http.StatusOK, obj{"books": booksList})
 }
 
 func (u *UserModule) CreateUser(data Registration, id bson.ObjectId) error {
